f4071e5db0abaf5b6f82d804bf28ec800b8741bb
chenxiaoyao6228 <chenxiaoyao6228@163.com>
2023-09-18 23:07:38 +0800

    feat: init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { Excalidraw } from "@excalidraw/excalidraw";
+
+// https://github.com/chenxiaoyao6228/cloudimg/blob/main/_Gragh/process.excalidraw
 
 function App() {
+  const [excalidrawData, setExcalidrawData] = useState(null);
+  // const [excalidrawAPI, setExcalidrawAPI] = useState(null);
+
+
+  useEffect(() => {
+
+    const url = window.location.search
+      .substring(1)
+      .replace(/\/\/github\.com/, "//raw.githubusercontent.com")
+      .replace(/\/blob\//, "/");
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        setExcalidrawData(data)
+
+      })
+      .catch(error => {
+        console.error('Error fetching JSON:', error);
+      });
+
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {excalidrawData ? <Excalidraw initialData={excalidrawData} 
+      // ref={(api) => setExcalidrawAPI(api)} 
+      /> : null}
     </div>
   );
 }
